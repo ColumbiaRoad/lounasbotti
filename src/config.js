56cc86c661881c39719ff664ad41b47afd94fcd0
Olli Jokinen <olli.jokinen@futurice.com>
2016-11-07 19:32:36 +0200

    Fixing

--- src/config.js
+++ src/config.js
@@ -10,10 +10,8 @@ const config = {
   ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   PROXY_URI: process.env.PROXY_URI,
-  WEBHOOK_URL: process.env.WEBHOOK_URL,
-  STARBOT_COMMAND_TOKEN: process.env.STARBOT_COMMAND_TOKEN,
   SLACK_TOKEN: process.env.SLACK_TOKEN,
-  ICON_EMOJI: ':stars:'
+  ICON_EMOJI: ':knife_fork_plate:',
 }
 
 module.exports = (key) => {
