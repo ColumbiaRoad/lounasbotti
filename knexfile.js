b23344460996e95a05f6d02624471e0493ca198f
Olli Jokinen <olli.jokinen@futurice.com>
2016-11-09 21:54:37 +0200

    Production knex configs

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,12 @@ module.exports = {
       max: 10,
     },
   },
+  production: {
+    client: 'postgresql',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
 };
