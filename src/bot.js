26ef35be319a4319131713bc1cb789e2812dee86
Olli Jokinen <olli.jokinen@futurice.com>
2016-11-09 21:43:37 +0200

    Add restaurant with command

--- src/bot.js
+++ src/bot.js
@@ -2,10 +2,17 @@ import winston from 'winston';
 import slack from 'slack';
 import _ from 'lodash';
 import config from './config';
+import Restaurant from './models/Restaurant';
 
 const bot = slack.rtm.client();
 
 let self;
+
+const message = {
+  token: config('SLACK_TOKEN'),
+  icon_emoji: config('ICON_EMOJI'),
+  username: 'Lounasbotti',
+};
 
 bot.started((payload) => {
   self = payload.self;
@@ -15,19 +22,21 @@ bot.message((msg) => {
   if (!msg.user) return;
   if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${self.id}>`)) return;
 
-  slack.chat.postMessage({
-    token: config('SLACK_TOKEN'),
-    icon_emoji: config('ICON_EMOJI'),
-    channel: msg.channel,
-    username: 'Lounasbotti',
-    text: 'Hello world',
-  }, (err, data) => {
-    if (err) throw err;
+  message.channel = msg.channel;
 
-    const txt = _.truncate(data.message.text);
+  if (msg.text.match(/lisää/i)) {
+    const restaurantName = msg.text.split(/lisää/i)[1].trim();
+    Restaurant.query().insert({ name: restaurantName }).then((restaurant) => {
+      message.text = `${restaurant.name} lisättiin`;
+      slack.chat.postMessage(message, (err, data) => {
+        if (err) throw err;
 
-    winston.log(`🤖  beep boop: I responded with "${txt}"`);
-  });
+        const txt = _.truncate(data.message.text);
+
+        winston.log(`🤖  beep boop: I responded with "${txt}"`);
+      });
+    });
+  }
 });
 
 module.exports = bot;
